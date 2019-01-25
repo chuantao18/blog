61cb2a351c43a53ee21355dc823143c32801dd15
wuyazi <wangchuantaoo@126.com>
2019-01-25 10:32:46 +0800

    blog skeleton

---
+++ src/main/java/com/my/blog/website/controller/IndexController.java
@@ -0,0 +1,7 @@
+package com.my.blog.website.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+}
---
+++ src/main/java/com/my/blog/website/service/Temp.java
@@ -0,0 +1,5 @@
+package com.my.blog.website.service;
+
+public interface Temp
+{
+}
